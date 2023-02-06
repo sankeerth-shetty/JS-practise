ca464dc4ef179fdadd8c52093fbce749b0f209cd
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-02-06 22:57:33 +0530

    js practise

---
+++ area and perimeter/script.js
@@ -0,0 +1,37 @@
+// area of a circle :-
+
+let a = parseFloat(prompt("enter radius (aoc)"));
+let r = a*a;
+let p = 3.14;
+alert(`area of circle is ${r*p}`);
+
+
+
+
+// perimeter of a circle (or can say [circumference of circle]) :- 
+
+// let a = parseFloat(prompt("enter radius (poc)"));
+// let p = 3.14;
+// alert(`perimeter of circle is ${2*p*a}`);
+
+
+
+
+// area of triangle :-
+
+// let a = parseFloat(prompt("enter base"));
+// let b = parseFloat(prompt("enter height"));
+// alert(`area of triangle is ${0.5*a*b}`);
+
+
+
+
+// perimeter of triangle :-
+
+// let a = parseFloat(prompt("enter number for 1st side"));
+// let b = parseFloat(prompt("enter number for 2nd side"));
+// let c = parseFloat(prompt("enter number for 3rd side"));
+// alert(`perimeter of triangle is ${a+b+c}`);
+
+
+
