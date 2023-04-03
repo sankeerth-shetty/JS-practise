b6de3d4a006ee736d8ee9dab7bbde6e2400b4d88
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-04-03 23:28:00 +0530

    js lecture

---
+++ js lectures/lecture12/script.js
@@ -0,0 +1,28 @@
+/*
+Variable
+Operator
+Data types
+decision control structure
+looping
+
+
+*/
+
+// let vr = document.getElementsByClassName('list-item');
+// console.log(vr);
+// for(let i=0;i<vr.length;i++)
+// {
+//     vr[i].style.backgroundColor = "#000";
+//     vr[i].style.color = "#fff";
+// }
+// vr[2].textContent = "Banana";
+
+//innertext
+//TextContent
+//innerHtml
+//node list
+//HTML Collection
+    
+// document.getElementById('this1').textContent = DocumentType;
+
+let text = document.getElementById('divA').textContent = 'This text is diffferent';
