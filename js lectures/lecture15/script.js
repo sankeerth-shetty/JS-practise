40e1c85beed5a002c0d798c421d0d80b0987239b
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-04-06 22:48:56 +0530

    lecture 15

---
+++ js lectures/lecture15/script.js
@@ -0,0 +1,16 @@
+// let arr =[1,2,3,4,5,6,7,8,9];
+// for(let i=0; i<arr.length; i++){
+//     if(i%2 == 0)
+//     {
+//         console.log(arr[i]);
+//         // console.log(i);
+//     }
+// }
+
+for(let i=0; i<5; i++){
+    for(let j=0; j<5; j++)
+    {
+        console.log('*');
+    }
+    console.log('\n');
+}
