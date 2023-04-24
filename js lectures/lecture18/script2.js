9faeabbf238db9fd687dce2d65f7836f6892f05e
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-04-24 23:16:58 +0530

    class and constructor

---
+++ js lectures/lecture18/script2.js
@@ -0,0 +1,87 @@
+// class Animal{
+//     display()
+//     {
+//         console.log('This is animal class');
+//     }
+// }
+
+// const obj = new Animal();
+// obj.display();
+
+
+// class Animal{
+//     detailsAnimal(name,age){
+//         console.log(`This is ${name} and age is ${age}`);
+//     }
+// }
+
+// const obj = new Animal();
+// obj.detailsAnimal('Tommy',5);
+
+
+// class Animal{
+//     constructor(name,age)
+//     {
+//         console.log(`Name of animal is ${name} and age is ${age} years`);
+//     }
+// }
+
+// const obj = new Animal('Ricky',6);
+
+
+// constructor : default constructor :
+
+
+// class Shape{
+//     constructor()
+//     {
+//         console.log('This is default constructor');
+//     }
+// }
+
+// const obj = new Shape();
+
+
+// class Shape{
+//     constructor(l,b)
+//     {
+//         let length = l;
+//         let breadth = b;
+//         let area = length * breadth;
+//         console.log(`Area of rectangle is ${area}`);
+//     }
+// }
+
+// const obj = new Shape(25,25);
+
+
+// class Shape{
+//     constructor(l,b,h)
+//     {
+//         this.l = l;
+//         this.b = b;
+//         this.h = h;
+//         let vol = l * b * h;
+//         console.log(`Volume of cuboid is :${vol}`);
+//     }
+// }
+
+// const obj = new Shape(1,2,3);
+
+
+// class Employee{
+//     constructor(name,age,salary)
+//     {
+//         this.name = name;
+//         this.age = age;
+//         this.salary = salary;
+//     }
+//     displayDetails()
+//     {
+//         console.log(`Details of employee is : `);
+//         console.log(`Welcome : ${this.name} your age is ${this.age} and your current salary is ${this.salary}`);
+//     }
+// }
+
+// const obj = new Employee('Sankeerth',21,28000);
+// obj.displayDetails();
