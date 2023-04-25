4dae39472510700ea393cf31c04277a80824ba0a
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-04-25 19:03:24 +0530

    inheritence

---
+++ js lectures/lecture18/script3.js
@@ -0,0 +1,29 @@
+// Inhertitence in Javascript 
+
+// class, Object, constructor
+
+// inheritance - single level - Parent class (super class), Child class (sub class)
+
+
+// Single level inheritance 
+
+// class Animal{
+//     constructor()
+//     {
+//         console.log("this is parent constructor");
+//     }
+// }
+
+// class Dog extends Animal{
+//     display()
+//     {
+//         console.log("this is child class");
+//     }
+// }
+
+// const obj = new Dog();
+// obj.display();
+
+
+
+
