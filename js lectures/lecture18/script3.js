8cfa55bfbb829d1fef9c8cf2e305a50d883478b7
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-04-26 23:22:04 +0530

    inheritence

--- js lectures/lecture18/script3.js
+++ js lectures/lecture18/script3.js
@@ -1,4 +1,4 @@
-// Inhertitence in Javascript 
+// Inheritence in Javascript 
 
 // class, Object, constructor
 
@@ -24,6 +24,69 @@
 // const obj = new Dog();
 // obj.display();
 
+
+//Multi level inheritence 
+
+// class Animal{
+//     sound()
+//     {
+//         console.log("every animal has different sound");
+//     }
+//     sleep()
+//     {
+//         console.log("every animal sleep");
+//     }
+// }
+// class Dog extends Animal{
+//     speak()
+//     {
+//         console.log("dog barks");
+//     }
+//     eat()
+//     {
+//         console.log("dog is eating");
+//     }
+// }
+// class PetDog extends Dog{
+//     play()
+//     {
+//         console.log("pet dogs are playing");
+//     }
+// }
+
+// const obj = new PetDog();
+// obj.eat();
+// obj.sleep();
+// obj.play();
+// obj.sound();
+// obj.speak();
+
+
+//To call get from parent constructor and display in child
+
+class Shape{
+    constructor(name)
+    {
+        this.name = name;
+        console.log(name);
+    }
+}
+
+class Rectangle extends Shape{
+    constructor(name,index)
+    {
+        super(name);
+        this.index = index;
+    }
+    display()
+    {
+        console.log(`Welcome ${this.name} your index is ${this.index}`);
+    }
+}
+
+const obj = new Rectangle("Ram",1);
+obj.display();
+const ab = new Shape();
 
 
 
