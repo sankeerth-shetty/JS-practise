147a6ffe7c1a7334b954aa712fd4bbd4321996df
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-05-05 17:16:28 +0530

    mini assessment1

---
+++ js mini assessments/script1.js
@@ -0,0 +1,25 @@
+class Cars {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+    }
+
+    myCarSpeed(){
+        console.log(`the current speed of car ${this.make} in km is ${this.speed}`);
+    }
+
+    accelerate(){
+        this.speed += 10;
+        console.log(`the accelerated speed is ${this.speed}`);
+    }
+
+    brake(){
+        this.speed -= 5;
+        console.log(`Speed after brake applied is ${this.speed}`);
+    }
+}
+
+const cars = new Cars("Bmw",120);
+cars.myCarSpeed();
+cars.accelerate();
+cars.brake();
