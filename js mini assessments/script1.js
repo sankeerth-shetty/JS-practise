21808516274aa27ce23e97f89c3adc22ca2040ad
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-05-06 17:25:49 +0530

    mini assessment1

--- js mini assessments/script1.js
+++ js mini assessments/script1.js
@@ -17,10 +17,32 @@ class Cars {
         this.speed -= 5;
         console.log(`Speed after brake applied is ${this.speed}`);
     }
+
+    getter(speedUs){
+        if(this.speed = speedUs){
+            this.speed = speedUs / 1.6;
+            console.log(`the speed of car in mi/h is ${this.speed}`);
+        }
+    }
+
+    setter(speedUs){
+        this.speed = speedUs * 1.6;
+        console.log(`the speed of car mi/h ${speedUs} converted from mi/h to km is ${this.speed}`);
+    }
 }
 
 const cars = new Cars("Bmw",120);
 cars.myCarSpeed();
 cars.accelerate();
 cars.brake();
+cars.getter(120);
+cars.setter(120);
 cars.myCarSpeed();
+
+// const cars2 = new Cars("Audi",140);
+// cars2.myCarSpeed();
+// cars2.accelerate();
+// cars2.brake();
+// cars2.getter(140);
+// cars2.setter(140);
+// cars2.myCarSpeed();
