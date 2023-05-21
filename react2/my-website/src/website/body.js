b89b4aee3617a94c13c084b5883d161fe531a6e0
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-05-21 22:36:15 +0530

    react basic2

--- react2/my-website/src/website/body.js
+++ react2/my-website/src/website/body.js
@@ -3,8 +3,17 @@ import img1 from './image.png'
 
 function Body(){
     return(
-        <div className='img-con'>
-            <img src={img1} className='image'/>
+        <div>
+            <div className='img-con'>
+                <img src={img1} className='image'/>
+            </div>
+            <div className='container row'>
+                <div className='content'>
+                    <h2 className='heading'>LOREM IPSUM DOLOR</h2>
+                    <h3 className='sub-head'>LOREM IPSUM DOLOR SIT AI</h3>
+                    <p className='main-con'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
         </div>
     );
 }
