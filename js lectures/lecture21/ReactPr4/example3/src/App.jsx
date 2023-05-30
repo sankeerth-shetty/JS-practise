871a615180f2f7289faae4bb2915320c9c537121
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-05-30 23:24:41 +0530

    react prac4

--- js lectures/lecture21/ReactPr4/example3/src/App.jsx
+++ js lectures/lecture21/ReactPr4/example3/src/App.jsx
@@ -1,23 +1,45 @@
-import './components/Post.css'
-import Post from './components/Post'
+// import './components/Post.css'
+// import Post from './components/Post'
 
-function App() {
-  return (
-    <main className="text">
-      <Post author="J K Rowling" body="Harry Potter"/>
-      <br/>
-      <Post author="Robert Kiyosaki" body="Rich Dad Poor Dad" />
-      <br/>
-      <Post author="Enid Blyton" body="Famous Five"/>
-      <br/>
-      <Post author="Stephen King" body="The Shining"/>
-      <br/>
-      <Post author="Ruskin Bond" body="The Blue Umbrella"/>
-      <br/>
-      <Post author="R K Narayan" body="Malgudi Days"/>
-      <br/>
-    </main>
-  )
-}
+// function App() {
+//   return (
+//     <main className="text">
+//       <Post author="J K Rowling" body="Harry Potter"/>
+//       <br/>
+//       <Post author="Robert Kiyosaki" body="Rich Dad Poor Dad" />
+//       <br/>
+//       <Post author="Enid Blyton" body="Famous Five"/>
+//       <br/>
+//       <Post author="Stephen King" body="The Shining"/>
+//       <br/>
+//       <Post author="Ruskin Bond" body="The Blue Umbrella"/>
+//       <br/>
+//       <Post author="R K Narayan" body="Malgudi Days"/>
+//       <br/>
+//     </main>
+//   )
+// }
 
-export default App
+// export default App
+
+// import Post from './components/Post'
+
+// function App() {
+//   return (
+//     <main>
+//       <Post author="J K Rowling" body="Harry Potter"/>
+//       <Post author="Robert Kiyosaki" body="Rich Dad Poor Dad" />
+//       <Post author="Enid Blyton" body="Famous Five"/>
+//       <Post author="Stephen King" body="The Shining"/>
+//       <Post author="Ruskin Bond" body="The Blue Umbrella"/>
+//       <Post author="R K Narayan" body="Malgudi Days"/>
+//     </main>
+//   )
+// }
+
+// export default App
+
+
+
+
+
--- js lectures/lecture21/ReactPr4/example3/src/components/Post.jsx
+++ js lectures/lecture21/ReactPr4/example3/src/components/Post.jsx
@@ -1,10 +1,33 @@
+// function Post(props){
+//     return(
+//         <div className="text">
+//             <p className="fs">{props.author}</p>
+//             <p className="fs">{props.body}</p>
+//         </div>
+//     )
+// }
+
+// export default Post;
+
+// function Post(props){
+//     return(
+//         <div className="text" style={{backgroundColor: "cyan"}}>
+//             <h3 className="fs">{props.author}</h3>
+//             <h2 className="fs">{props.body}</h2>
+//         </div>
+//     )
+// }
+
+// export default Post;
+
+import postCss from './Post.module.css';
 function Post(props){
     return(
-        <div className="text">
-            <p className="fs">{props.author}</p>
-            <p className="fs">{props.body}</p>
+        <div className={postCss.post}>
+            <p className={postCss.author}>{props.author}</p>
+            <p className={postCss.text}>{props.body}</p>
         </div>
     )
-}
+} 
 
 export default Post;
