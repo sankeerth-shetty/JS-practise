68711db63b238b9c1e61f6704454a33b18b8d148
sankeerth-shetty <shetty.sankeerth@gmail.com>
2023-06-01 17:28:49 +0530

    props and states

---
+++ js lectures/lecture20/script4.js
@@ -0,0 +1,39 @@
+// props and states in ReactJs
+
+// Spread operator :
+// Rest operator :
+
+// -> Spread Operator :
+
+// function addition (x,y,z,p)
+// {
+//     return x+y+z+p;
+// }
+
+// let res = addition (25,25,25,25);
+// let res = addition ([25,25,25,25]);
+
+// const vl = [25,25,25,25];
+// let res = addition(...vl);
+// console.log(res);
+
+// function addition (x,y,z)
+// {
+//     return x+y+z;
+// }
+
+// let res = addition (25,25,25,25);
+// console.log(res);
+
+// let total = 0;
+// function addition(...args){
+//     for (const x of args)
+//     {
+//         total = total + x
+//     }
+//     return total;
+// }
+
+// let res = addition (25,50,50,50);
+// console.log(res);
+
